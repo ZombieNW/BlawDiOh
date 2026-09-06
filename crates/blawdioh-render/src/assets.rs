a5810fbc6c32f1e07670fee98a800e9742c0c0be
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-09-06 13:14:19 -0700

    Add error handling to asset bundler

--- crates/blawdioh-render/src/assets.rs
+++ crates/blawdioh-render/src/assets.rs
@@ -1,8 +1,9 @@
-use std::{collections::HashMap, error::Error, path::Path};
+use std::{collections::HashMap, error::Error, fs::exists, path::Path};
 
 use blawdioh_engine::types::{EyeState, MouthState};
 use image::RgbaImage;
 
+#[derive(Debug)]
 pub struct AssetBundle {
     pub base_texture: RgbaImage,
     pub mouths: HashMap<MouthState, RgbaImage>,
@@ -10,14 +11,18 @@ pub struct AssetBundle {
 }
 
 pub fn load_assets(dir: &Path) -> Result<AssetBundle, Box<dyn Error>> {
-    let base_texture = image::open(dir.join("base.png"))?.to_rgba8();
+    let base_texture = image::open(dir.join("base.png"))
+        .expect("Base texture not found")
+        .to_rgba8();
 
     let mut mouths = HashMap::new();
     let mouth_dir = dir.join("mouths");
 
     for state in MouthState::ALL {
         let file_path = mouth_dir.join(format!("{}.png", state.filename_stem()));
-        let sprite = image::open(&file_path)?.to_rgba8();
+        let sprite = image::open(&file_path)
+            .expect("Mouth texture not found")
+            .to_rgba8();
         mouths.insert(state, sprite);
     }
 
@@ -26,7 +31,9 @@ pub fn load_assets(dir: &Path) -> Result<AssetBundle, Box<dyn Error>> {
 
     for state in EyeState::ALL {
         let file_path = eye_dir.join(format!("{}.png", state.filename_stem()));
-        let sprite = image::open(&file_path)?.to_rgba8();
+        let sprite = image::open(&file_path)
+            .expect("Eye texture not found")
+            .to_rgba8();
         eyes.insert(state, sprite);
     }
 
